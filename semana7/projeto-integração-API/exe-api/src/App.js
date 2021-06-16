00fdc252d6809f4260b048b69758de8df3a76564
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-06-15 23:58:07 -0300

    e

--- semana7/projeto-integração-API/exe-api/src/App.js
+++ semana7/projeto-integração-API/exe-api/src/App.js
@@ -1,83 +1,96 @@
-import React from "react";
-import axios from "axios";
+import styled from 'styled-components'
+import axios from 'axios'
+import React from 'react'
+import Usuarios from './components/Usuarios'
+
+const Container=styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  margin-top: 5%;
+  margin-right: 40%;
+  margin-left: 40%;
+`
+
 
 const url =
-  "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
+  "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
 const headers = {
   headers: {
     Authorization: "carlos-azeredo-molina"
   }
-};
+}
 
 export default class App extends React.Component {
   state = {
-    users: [],
+    arrayNomes: [],
     inputName: "",
     inputEmail:"",
-    mostarlista:false
-  };
+    trocaTela: false
+  }
   componentDidMount() {
-    this.pegarPlaylists();
+    this.pegarUsuario()
   }
 
-  mudaInputPlaylist = (event) => {
-    this.setState({ inputPlaylist: event.target.value });
-  };
 
-  pegarPlaylists = () => {
-    axios
-      .get(url, headers)
-      .then((res) => {
-        this.setState({ playlists: res.data.result.list });
-      })
-      .catch((err) => {
-        alert(err.response.data.message);
-      });
-  };
+onChangeName = (event)  => {
+this.setState({inputName: event.target.value})
+}
+ onChangeEmail =(event) => {
+   this.setState({inputEmail:event.target.value})
+ }
 
-  criarPlaylist = () => {
-    const body = {
-      name: this.state.inputPlaylist
-    };
+ pegarUsuario=()=>{
+  axios.get(url,headers)
+  .then((resposta)=>{
+    console.log(resposta.data)
+    this.setState({arrayNomes:resposta.data})
+  })
+  .catch((err)=>{
+    
+  })
+}
 
-    axios
-      .post(url, body, headers)
-      .then((res) => {
-        alert("Usuário cadastrada com sucesso!");
-        this.setState({ inputPlaylist: "" });
-        this.pegarPlaylists();
-      })
-      .catch((err) => {
-        alert(err.response.data.message);
-      });
-  };
-
+criarUsuario=()=>{
+  const body={
+   name:this.state.inputName,
+    email:this.state.inputEmail
+  }
+  axios.post(url,body,headers)
+  .then(()=>{
+    alert("playlist cadastrada com sucesso")
+    this.setState({inputName:""})
+    this.pegarUsuario()
+  })
+  .catch((err)=>{
+    console.log(err.response.data)
+  })
+}
+onClickTroca=()=>{
+  this.setState({trocaTela:!this.state.trocaTela})
+}
   render() {
-    const componentesPlaylist = this.state.playlists.map((play) => {
-      return <li key={play.id}>{play.name}</li>;
-    });
+   
+    
 
-    // const musicas = [
-    //   { id: 1, nome: "Samba" },
-    //   { id: 2, nome: "Rock" }
-    // ];
-
-    // const musicasComponentes = [
-    //   <li>Samba</li>,
-    //   <li>Rock</li>
-    // ]
-
-    return (
+    return(
       <div>
-        <h1>Lista de Playlists</h1>
-        <input
-          value={this.state.inputPlaylist}
-          onChange={this.mudaInputPlaylist}
-        />
-        <button onClick={this.criarPlaylist}>Salvar</button>
-        {componentesPlaylist}
+         {this.state.trocaTela? <Usuarios arrayNomes={this.state.arrayNomes}/> :<div> <button onClick={this.onClickTroca} >Ir para página de lista </button>
+     
+        <Container>
+
+        <label>Nome</label>
+        <input value={this.state.inputName} onChange={this.onChangeNome}/> 
+        <label >E-mail</label>
+        <input value={this.state.inputEmail} onChange={this.onChangeEmail} /> 
+        <button onClick={this.criarUsuario}>Salvar</button>
+      </Container>
+      
       </div>
-    );
+  }
+      </div>
+    )
   }
 }
 
