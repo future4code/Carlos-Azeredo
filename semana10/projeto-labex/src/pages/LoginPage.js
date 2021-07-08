b030b72589d98f97c5c3f8acf97b98ef626a3bf4
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-07-08 13:57:27 -0300

    Integrações com a API loginPage e tripdetailsPage

--- semana10/projeto-labex/src/pages/LoginPage.js
+++ semana10/projeto-labex/src/pages/LoginPage.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { MainContainer, InputsContainer, StyledInputs, ButtonDiv, InfoDiv, StyledButton } from './styled'
 
 export default function LoginPage() {
+     const [email, setEmail] = useState("")
+     const [password, setPassword] = useState("")
     
-    const history = useHistory()
+    const onChangeEmail = (event) => {
+        setEmail(event.target.value)
+    }
+    const onChangePassword = (event) => {
+        setPassword(event.target.value)
+    }
+    const onSubmitLogin = () => {
+        console.log (email, password)
+        const body = {
+            email:email,
+            password:password
+        }
+        axios.post ('https://us-central1-labenu-apis.cloudfunctions.net/labeX/carlos-azeredo-molina/login', body)
+        .then((response) => {
+            console.log('Deu certo: ', response.data.token)
+            localStorage.setItem('token', response.data.token)
+            history.push('/')
+        }).catch((error) => {
+            console.log('Deu errado: ',error.response)
+
+        })
+    } 
+
+
+
+     const history = useHistory()
 
     const goBack = () => {
         history.goBack()
@@ -13,17 +41,19 @@ export default function LoginPage() {
     const renderAdminHomePage = () => {
         history.push("AdminHomePage")
     }
+
+
     return (
         <MainContainer>
             <InfoDiv>
             <h1>Login</h1>
             <InputsContainer>
-                <StyledInputs placeholder={'email'} />
-                <StyledInputs placeholder={'senha'} type={'password'} />
+                <StyledInputs placeholder={'email'} type="email" value={email} onChange={onChangeEmail}/>
+                <StyledInputs placeholder={'senha'} type={'password'} value={password} onChange={onChangePassword} />
             </InputsContainer>
             <ButtonDiv>
                 <StyledButton onClick={goBack}>Voltar</StyledButton>
-                <StyledButton onClick={renderAdminHomePage}>Entrar</StyledButton>
+                <StyledButton onClick={onSubmitLogin}>Entrar</StyledButton>
             </ButtonDiv>
             </InfoDiv>
         </MainContainer>
--- semana10/projeto-labex/src/pages/TripDetailsPage.js
+++ semana10/projeto-labex/src/pages/TripDetailsPage.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import {MainContainer} from './styled'
 
 export default function  TripDetailsPage(){
+useEffect (() => {
+    const token = localStorage.getItem('token')
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/carlos-azeredo-molina/trip/4xxJGzMigfaXEhGr5lMZ', {
+     headers: {
+         auth:token
+     } 
+    })
+    .then((response) => {
+        console.log(response.data)
+    }).catch ((error) => {
+        console.log('Deu erro:', error.response)
+    })
+},[])
+
+
+
 return (
     <MainContainer>
         TripDetailsPage
