f0644ecfa72e0c18cb5dbc947dffbef35eedd1cf
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-07-22 20:03:29 -0300

    Criação de Pages e de Rotas

---
+++ semana11/labeddit/src/routes/Router.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import SignUpPage from '../pages/SignUpPage/SignUpPage'
+import FeedPage from '../pages/FeedPage/FeedPage'
+import PostPage from '../pages/PostPage/Postpage'
+import ErrorPage from '../pages/ErrorPage/Errorpage'
+
+const Router = () => {
+
+    return (
+        <BrowserRouter>
+            <Switch>
+
+                <Route exact path={'/login'}>
+                    <LoginPage />
+                </Route>
+
+                <Route exact path={'/cadastrar'}>
+                    <SignUpPage />
+                </Route>
+
+                <Route exact path={'/'}>
+                    <FeedPage />
+                </Route>
+
+                <Route exact path={'/post/:id'}>
+                    <PostPage />
+                </Route>
+
+                <Route>
+                    <ErrorPage />
+                </Route>
+
+            </Switch>
+
+        </BrowserRouter>
+    )
+
+
+
+
+}
+
+export default Router
