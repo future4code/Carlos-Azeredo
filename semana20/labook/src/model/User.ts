bc6c16836e3dbef815b1cb70476549eaaa7f2393
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-09-20 09:20:30 -0300

    Labbok

---
+++ semana20/labook/src/model/User.ts
@@ -0,0 +1,18 @@
+export interface User {
+  id: string
+  name: string
+  email: string
+  passwordHash: string
+}
+
+
+export interface SignupDTO {
+  name: string
+  email: string
+  password: string
+};
+
+export interface LoginDTO {
+  email: string
+  password: string
+};
