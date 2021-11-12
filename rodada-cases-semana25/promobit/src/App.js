fe42a0e606f3e48d921da07435dcc56bfd99628e
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-11-12 10:10:05 -0300

    Instalação de Libs e criação de Rotas

--- rodada-cases-semana25/promobit/src/App.js
+++ rodada-cases-semana25/promobit/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Router from './route/Router'
+import { createGlobalStyle } from 'styled-components'
+
 
 function App() {
+
+  const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <Router />
     </div>
   );
 }
 
-export default App;
+export default App
