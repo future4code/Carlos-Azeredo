79e55d2b9bb4ce6967c570588490043ef0e5c573
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-11-15 12:36:19 -0300

    Paginação e Header

--- rodada-cases-semana25/promobit/src/pages/HomePage/HomePage.js
+++ rodada-cases-semana25/promobit/src/pages/HomePage/HomePage.js
@@ -1,21 +1,60 @@
 import React, { useContext } from "react";
+import Header from "../../components/Header/Header";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import GlobalStateContext from "../../global/GlobalStateContext";
-import Header from "../../components/Header/Header"
-import { CardsContainer } from "./styled";
+import { ButtonsContainer, CardsContainer, StyledButton } from "./styled";
 
 
 const HomePage = () => {
-    const { movies } = useContext(GlobalStateContext)
-  
-    return (
-      <div>
-        <Header />
-        <CardsContainer>
-          <MovieCard movies={movies} />
-        </CardsContainer>
-      </div>
-    );
+  const { movies, currentPage, setCurrentPage } = useContext(GlobalStateContext)
+  console.log(currentPage)
+
+  function scrollToTop() {
+    window.scrollTo(0, 0);
   }
-  
-  export default HomePage;
+
+  const nextPage = () => {
+    if (Number(currentPage) < 500) {
+      setCurrentPage((Number(currentPage) + 1).toString())
+      scrollToTop()
+    }
+  }
+
+  const previousPage = () => {
+    if (Number(currentPage) > 1) {
+      setCurrentPage((Number(currentPage) - 1).toString())
+      scrollToTop()
+    }
+  }
+
+  const firstPage = () => {
+    if (Number(currentPage) > 1) {
+      setCurrentPage("1")
+      scrollToTop()
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <CardsContainer>
+        <MovieCard movies={movies} />
+      </CardsContainer>
+
+      {
+        currentPage == 1 ?
+          <ButtonsContainer>
+            <StyledButton onClick={() => nextPage()}>Próxima Página</StyledButton>
+          </ButtonsContainer> :
+          <ButtonsContainer>
+            <StyledButton onClick={() => firstPage()}>Primeira Página</StyledButton>
+            <StyledButton onClick={() => previousPage()}>Página Anterior</StyledButton>
+            <StyledButton onClick={() => nextPage()}>Próxima Página</StyledButton>
+          </ButtonsContainer>
+      }
+
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ rodada-cases-semana25/promobit/src/pages/MovieDetails/styled.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-wrap:wrap;
+    padding:12px;
+    @media (max-width: 699px){
+        padding:12px;
+    }
+`
+
+export const TextContainer = styled.div`
+    text-align:start;
+    width:32vw;
+    margin-left:40px;
+    color:#99AABB;
+    h1, h3{
+        color:#FFFFFF;
+    }
+    @media (max-width: 699px){
+        text-align:start;
+        width:80%;
+        margin-left:0px;
+    }
+`
+
+export const Card = styled.div`
+    width:350px;
+    height:520px;
+    margin-bottom:10px;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+    &:hover{
+        cursor: pointer;
+    }
+`
+
+export const MovieImg = styled.div`
+    width:100%;
+    height:100%;
+    background-size: cover;
+    background-position: center;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-Content: flex-end;
+`
